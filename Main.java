bf63884e0fa9b98944b8460b0bc17e3afb186aa5
mano rajesh <mano@raje.sh>
2020-08-25 18:46:00 +0000

    Fixed switch-case, added and polished factorial

--- Main.java
+++ Main.java
@@ -1,54 +1,53 @@
-  /*
-    - The program should have a separate class that contains a function to get and return
-    input.
-    - The program should have another class that contains all the messages and prompts
-    that the program will print out.
-    - There should be NO immediate strings in ANY other classes. Whenever you
-    print out a message, reference a string in this class.
+/*
+  - The program should have a separate class that contains a function to get and return
+  input.
+  - The program should have another class that contains all the messages and prompts
+  that the program will print out.
+  - There should be NO immediate strings in ANY other classes. Whenever you
+  print out a message, reference a string in this class.
 
-    - The program should have yet another class that contains a function to calculate and
-    return factorials.
-    
-    -----------------------------------------------------------------------------------------
-    UI Goal:
-
-    Select one of the following options:
-    (1) Arithmetic
-    (0) More
-    > 1
-
-    Select one of the following operations:
-    (+) Addition
-    (-) Subtraction
-    (*) Multiplication
-    (/) Division
-    > +
-
-    Enter operand 1:
-    > 1
-
-    Enter operand 2:
-    > 2
-
-    Result: 1 + 2 = 3
-    
-    Select one of the following options:
-    (1) Arithmetic
-    (0) More
-    > 0
-    
-    Select one of the following functions:
-    (!) Factorial
-    (2) Square Root
-    (3) Cube Root
-    > !
-
-    Enter operand:
-    > 4
-
-    Result: 4! = 24
-  */
+  - The program should have yet another class that contains a function to calculate and
+  return factorials.
   
+  -----------------------------------------------------------------------------------------
+  UI Goal:
+
+  Select one of the following options:
+  (1) Arithmetic
+  (0) More
+  > 1
+
+  Select one of the following operations:
+  (+) Addition
+  (-) Subtraction
+  (*) Multiplication
+  (/) Division
+  > +
+
+  Enter operand 1:
+  > 1
+
+  Enter operand 2:
+  > 2
+
+  Result: 1 + 2 = 3
+  
+  Select one of the following options:
+  (1) Arithmetic
+  (0) More
+  > 0
+  
+  Select one of the following functions:
+  (!) Factorial
+  (2) Square Root
+  (3) Cube Root
+  > !
+
+  Enter operand:
+  > 4
+
+  Result: 4! = 24
+*/
 
 class Main {
   public static void main(String[] args) {
@@ -56,21 +55,21 @@ class Main {
       System.out.format(Msg.STARTMSG);
       System.out.print(Msg.CARROT);
 
-      String character = get.getchar();
-      switch (character)
+      String character = get.getstr();
+      switch (character)                              // Checking if Arithmetic or More is selected
       {
-        case character.equals(Msg.ONE):
+        case Msg.ONE:
         System.out.format(Msg.ARITHMETIC);
 
         System.out.print(Msg.CARROT);
-        character = get.getchar();;
+        character = get.getstr();;
 
-        switch (character)
+        switch (character)                            // Checking which operation is selected, with a case for each
         {
-          case character.equals(Msg.PLUS):
+          case Msg.PLUS:
           System.out.format(Msg.OP1);
           System.out.print(Msg.CARROT);
-
+ 
           double op1 = get.getdouble();
 
           System.out.format(Msg.OP2);
@@ -83,7 +82,7 @@ class Main {
           System.out.format(Msg.SUM , op1, op2, result);
           break;
 
-          case character.equals(Msg.SUB):
+          case Msg.SUB:
           System.out.format(Msg.OP1);
           System.out.print(Msg.CARROT);
 
@@ -99,7 +98,7 @@ class Main {
           System.out.format(Msg.DIFF, op1, op2, result);
           break;
 
-          case character.equals(Msg.MULTI):
+          case Msg.MULTI:
           System.out.format(Msg.OP1);
           System.out.print(Msg.CARROT);
 
@@ -115,7 +114,7 @@ class Main {
           System.out.format(Msg.PRO, op1, op2, result);
           break;
 
-          case character.equals(Msg.DIV):
+          case Msg.DIV:
           System.out.format(Msg.OP1);
           System.out.print(Msg.CARROT);
 
@@ -133,40 +132,40 @@ class Main {
         }
         break;
 
-        case character.equals(Msg.ZERO):
+        case Msg.ZERO:
         System.out.format(Msg.MORE);
         System.out.print(Msg.CARROT);
         
-        character = get.getchar();
+        character = get.getstr();
         switch (character)
         {
-          case character.equals(Msg.EX):
+          case Msg.EX:                                // Checking which operation is selected
           System.out.format(Msg.OP);
           System.out.print(Msg.CARROT);
 
-          int num1 = get.getint();
+          double num1 = get.getdouble();
           
-          int result = cal.factorial(num1);
+          double result = cal.factorial(num1);
 
           System.out.format(Msg.FACT, num1, result);
           break;
 
-          case character.equals(Msg.TWO):
+          case Msg.TWO:
           System.out.format(Msg.OP);
           System.out.print(Msg.CARROT);
 
-          num1 = get.getint();
+          num1 = get.getdouble();
           
           result = cal.square(num1);
 
           System.out.format(Msg.SQUARE, num1, result);
           break;
 
-          case character.equals(Msg.THREE):
+          case Msg.THREE:
           System.out.format(Msg.OP);
           System.out.print(Msg.CARROT);
 
-          num1 = get.getint();
+          num1 = get.getdouble();
           
           result = cal.cube(num1);
 
@@ -178,4 +177,3 @@ class Main {
     }
   }
 }
-      
